77fc86fedc8b1c4d601d5bb0faf72e136fc914b1
Silvia Sanz <silviasanz3@yahoo.fr>
2018-10-22 23:21:50 +0200

    quetes

---
+++ quete-film.js
@@ -0,0 +1,7 @@
+const filmName = 'E.T';
+const yearEdit = '1982';
+const realisatorName = 'S.Spielberg';
+const message =  filmName + ' réalisé par ' + realisatorName + ', est sorti en '+ yearEdit;
+
+// Display a popup
+alert(message);
